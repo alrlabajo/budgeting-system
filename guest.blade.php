b933c418318092305ce4109e754c0afbd2405d61
alrlabajo <150265116+alrlabajo@users.noreply.github.com>
2023-12-04 22:43:27 +0800

    Update guest.blade.php

--- guest.blade.php
+++ guest.blade.php
@@ -8,78 +8,52 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans text-gray-900 antialiased">
         <!--Background Color-->
         <div class="w-screen h-screen relative bg-indigo-800">
         <!--Background Photo-->
-            <img class="w-[1304px] h-screen left-[616px] top-[-5px] absolute" src="{{ asset('css/images/img/background.png') }}"/>
+            <img class="w-[1367px] h-screen left-[340px] top-[0px] absolute" src="{{ asset('css/image 2.png') }}"/>
             <!--Form Container-->
-            <div class="w-[760.57px] h-[977.88px] left-[74px] top-[69px] absolute">
-                <div class="w-[760.57px] h-[977.88px] left-0 top-0 absolute bg-white rounded-xl">
+            <div class="w-[550px] h-[600px] left-[74px] top-[52px] absolute">
+                <div class="w-[550px] h-[720.88px] left-0 top-0 absolute bg-white rounded-xl">
                     <div class="w-96 h-96 left-[131.13px] top-[229.80px] absolute rounded-xl flex-col justify-start items-start gap-12 inline-flex">
-                        <div class="flex-col justify-start items-center gap-11 flex">
+                        <div class="flex-col justify-start items-center gap-11 flex" style="position: absolute; left: -46px; top: -100px;">
                             <!--Headings-->
-                            <div class="flex-col justify-start items-center gap-7 flex">
-                            <div class="w-96 text-cyan-700 text-6xl font-bold font-['Inter'] leading-9">PLM Module</div>
-                            <div class="w-96 text-cyan-700 text-4xl font-medium font-['Inter'] leading-9">Sign In</div>
+                            <div class="flex-col justify-start items-center gap-3 flex">
+                            <div class="w-96 text-cyan-700 text-5xl font-bold font-['Inter'] leading-9">PLM Module</div>
+                            <div class="w-96 text-cyan-700 text-3xl font-medium font-['Inter'] leading-9">Sign In</div>
                         </div>
                         <div class="flex-col justify-start items-center gap-6 flex">
                             <div class="flex-col justify-start items-start gap-6 flex">
-                                <div class="flex-col justify-start items-start gap-6 flex">
-                                    <!--Email Address-->
-                                    <div class="flex-col justify-start items-start gap-2.5 flex">
-                                        <div class="w-96 h-7 text-zinc-900 text-xl font-normal font-['Inter'] leading-9">Email Address</div>
-                                        <div class="w-96 justify-start items-start inline-flex">
-                                            <input class="grow shrink basis-0 h-12 px-4 bg-white rounded-md border border-slate-500 flex-col justify-center items-center gap-2.5 inline-flex"></input>
-                                        </div>
-                                    </div>
-                                    <!--Password-->
-                                    <div class="flex-col justify-start items-start gap-2.5 flex">
-                                        <div class="w-96 justify-between items-center inline-flex">
-                                            <div class="w-48 h-7 text-zinc-900 text-xl font-normal font-['Inter'] leading-9">Password</div>
-                                            <a class="w-52 text-right text-slate-500 text-base font-normal font-['Inter'] leading-9 hover:underline" href="{{ route('password.request') }}">Forgot Password?</a>
-                                        </div>
-                                        <div class="w-96 justify-start items-start inline-flex">
-                                            <input class="grow shrink basis-0 h-12 pl-4 pr-12 bg-white rounded-md border border-slate-500 flex-col justify-center items-center gap-2.5 inline-flex"></input>
-                                            <div class="w-12 h-12 left-[452px] top-0 absolute justify-center items-center gap-2.5 flex">
-                                                <div class="w-4 h-4 relative"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="flex-col justify-start items-start gap-5 flex">
-                                    <div class="justify-start items-center gap-5 inline-flex">
-                                        <div class="w-6 h-6 relative">
-                                            <input id="default-checkbox" type="checkbox" value="" class="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-white dark:border-gray-600">
-                                        </div>
-                                        <div class="w-72 text-zinc-900 text-base font-light font-['Inter'] leading-9">Keep me signed in</div>
-                                    </div>
-                                    <div class="w-96 h-14 px-5 py-2.5 bg-slate-500 rounded shadow justify-center items-center inline-flex hover:bg-blue-900">
-                                        <div class="text-white text-2xl font-bold font-['Inter'] leading-normal">Login</div>
-                                    </div>
+                                <!--Login Form-->
+                                <div id="content">
+                                    @yield('login')
                                 </div>
                             </div>
+                            <!--Signed in with-->
                             <div class="flex-col justify-start items-center flex">
-                                <div class="w-96 h-px opacity-25 border border-slate-600"></div>
-                                <div class="p-2.5 bg-white justify-start items-start gap-2.5 inline-flex">
-                                    <div class="w-36 text-center text-zinc-400 text-lg font-normal font-['Inter'] leading-normal">or sign in with</div>
+                                <div class="w-96 h-px opacity-25 border border-slate-600 my-4" style="position: absolute; left: 0px; top: 462px"></div>
+                                <div class="w-40 h-11 p-2.5 bg-white justify-start items-start gap-2.5 inline-flex">
+                                    <label class="w-36 text-center text-zinc-400 text-lg font-normal font-['Inter'] leading-normal bg-white">or sign in with</label>
                                 </div>
                             </div>
-                            <div class="w-96 h-14 flex items-center justify-center bg-gray-200 rounded hover:bg-slate-700">
-                                <img class="w-6 h-7 left-[124.89px] top-[16.24px]" src="{{ asset('css/images/img/image 1.png') }}" /></div>
-                                <button class="text-center text-slate-600 text-xl font-normal font-['Inter'] leading-7 ml-4">Continue with Microsoft</button>
-                            </div>
+                            <!--Continue with Microsoft-->
+                            <button type="submit" class="w-96 h-14 flex items-center justify-center bg-gray-200 rounded text-[#4B5768]">
+                                <img class="w-6 h-7 left-[124.89px] top-[10px]" src="{{ asset('css/image 1.png') }}" />
+                                <div class="text-center text-slate-600 text-xl font-normal font-['Inter'] leading-7 ml-4">{{ __('Continue with Microsoft') }}</div>
+                            </button>
                         </div>
                     </div>
                 </div>
-                <div class="w-96 h-20 left-[131.13px] top-[102.41px] absolute">
-                    <img class="w-20 h-20 left-0 top-0 absolute rounded-xl" src="{{ asset('css/images/img/image 5.png') }}" />
-                    <img class="w-80 h-14 left-[99.91px] top-[17.48px] absolute" src="{{ asset('css/images/img/image 4.png') }}" />
+                <div class="w-96 h-20 left-[60px] top-[20px] absolute">
+                    <img class="w-20 h-20 left-1 top-1.5 absolute rounded-xl" src="{{ asset('css/image 5.png') }}" />
+                    <img class="w-75 h-15 left-[93px] top-[17.48px] absolute" src="{{ asset('css/image 4.png') }}" />
                 </div>
             </div>
         </div>
